62b4e25a53437c4d1a941ce8f1f561bec45c72c5
Raghava <91364250+Raghava0684@users.noreply.github.com>
2022-05-20 12:33:08 +0530

    Update AddressController.java

--- src/main/java/com/cigniti/foodApp/controller/AddressController.java
+++ src/main/java/com/cigniti/foodApp/controller/AddressController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = "https:20.235.64.55:3000")
+@CrossOrigin(origins = "http:20.219.35.173:3000")
 @RestController
 
 @RequestMapping("/address")
